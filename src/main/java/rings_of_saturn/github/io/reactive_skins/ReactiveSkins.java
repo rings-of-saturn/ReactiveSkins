a1857cf504c1180497b450137171b8745cf877fd
TLS <thecolorizedsystem@gmail.com>
2024-09-25 18:26:47 +0300

    ⭐Initial Commit!

---
+++ src/main/java/rings_of_saturn/github/io/reactive_skins/ReactiveSkins.java
@@ -0,0 +1,10 @@
+package rings_of_saturn.github.io.reactive_skins;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ReactiveSkins implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/rings_of_saturn/github/io/reactive_skins/client/ReactiveSkinsClient.java
@@ -0,0 +1,14 @@
+package rings_of_saturn.github.io.reactive_skins.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ReactiveSkinsClient implements ClientModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("Reactive Skins");
+    public static final String MOD_ID = "assets/reactive_skins";
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
